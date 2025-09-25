cc9dd6fdbe5a285333b3aeba21e2f4e1f0586359
unknown <niharikaflyhigh@gmail.com>
2025-09-25 11:15:39 +0530

    trainer

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 import dill as pickle
+from sklearn.metrics import r2_score    
  
 
 def save_object(file_path, obj):
@@ -11,4 +12,20 @@ def save_object(file_path, obj):
     with open(file_path, 'wb') as file_obj:
      pickle.dump(obj, file_obj)
         
-   
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    report = {}
+    for i in range(len(models)):
+        model = list(models.values())[i]
+        print(f'Model Name: {model}')
+        # Train model
+        model.fit(X_train, y_train)
+
+        # Predict Testing data
+        y_test_pred = model.predict(X_test)
+
+        # Get R2 scores
+        test_model_score = r2_score(y_test, y_test_pred)
+        print(f'R2 score: {test_model_score}')
+
+        report[list(models.keys())[i]] = test_model_score
+    return report   
